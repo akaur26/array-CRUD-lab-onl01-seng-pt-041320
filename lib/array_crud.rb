ff0ae996ded170bde97b81abd27d3482f55e2e9f
Anmol Kaur <kaur.anmol26@gmail.com>
2020-04-18 02:39:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,25 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+ ["I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+   ["I", "am", "really", "learning", "arrays!"]
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  ["wow", I", "am", "really", "learning", "arrays!"]
 end
 
 def remove_element_from_end_of_array(array)
-  
+  "arrays!"
 end
 
 def remove_element_from_start_of_array(array)
-  
+  "wow"
 end
 
 def retrieve_element_from_index(array, index_number)
